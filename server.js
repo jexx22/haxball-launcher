d5298c89ab9a0e6b790cd08274524c077c5b2f1e
jexx22 <jesusvhr14@gmail.com>
2026-09-07 18:45:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
 
 const app = express();
 
 app.use('/', createProxyMiddleware({
   target: 'https://www.haxball.com',
   changeOrigin: true,
-  ws: true
+  ws: true,
+  secure: false,
+  selfHandleResponse: true,
+  onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    
+    let response = responseBuffer.toString('utf8');
+    response = response.replace(/https:\/\/www\.haxball\.com/g, '');
+    return response;
+  })
 }));
 
 const PORT = process.env.PORT || 3000;
